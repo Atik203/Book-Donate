e4bc3baacf5051045ae9465758d2151d89f491c3
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-08 17:05:09 +0600

    update backend

--- backend/book/urls.py
+++ backend/book/urls.py
@@ -2,13 +2,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import BookViewSet, GenreViewSet
+from .views import BookViewSet, GenreViewSet, PopularBooksView
 
 router = DefaultRouter()
 
 router.register('list', BookViewSet)
 router.register('genre', GenreViewSet)
-
+router.register('popular', PopularBooksView, basename='popular-books')
 urlpatterns =[
     path('', include(router.urls))
 ]
