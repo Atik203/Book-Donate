9ed7c3725d75693f5484784b7cca66c7d749fcbd
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-11 00:06:20 +0600

    updated review card style

--- frontend/src/components/UserReview/UserReview.tsx
+++ frontend/src/components/UserReview/UserReview.tsx
@@ -5,6 +5,7 @@ export interface UserReviewProps {
     email: string;
     first_name: string;
     last_name: string;
+    image: string;
   };
   book: {
     title: string;
@@ -19,21 +20,31 @@ const UserReview = ({ review }: { review: UserReviewProps }) => {
   const { user, book, rating, comment, date_added } = review;
 
   return (
-    <div className="bg-white p-4 text-center rounded-md shadow-xl mx-2 w-64 h-52">
-      <h3 className="text-xl font-bold text-gray-800">
-        {user.first_name} {user.last_name}
-      </h3>
-      <p className="text-gray-600">
-        <em>{user.username}</em>
-      </p>
+    <div className="bg-white p-4 text-center rounded-md shadow-xl mx-2 w-64 min-h-48">
+      <div className="flex justify-center gap-2 my-1">
+        <img
+          src={user.image}
+          alt={user.username}
+          className="w-12 h-12 rounded-full mx-auto"
+        />
+        <div className="text-start">
+          <p className="text-gray-800">
+            {user.first_name} {user.last_name}
+          </p>
+          <p className="text-gray-500 text-xs">{user.username}</p>
+        </div>
+      </div>
       <p className="text-gray-800">
         <strong>{book.title}</strong> by {book.author}
       </p>
-      <p className="text-yellow-500">{rating}</p>
-      <p className="text-gray-700">{comment}</p>
-      <p className="text-gray-500 text-sm">
-        {new Date(date_added).toLocaleDateString()}
-      </p>
+
+      <p className="text-gray-700 ">{comment}</p>
+      <div className="flex items-center justify-between my-1">
+        <p className="">{rating}</p>
+        <p className="text-gray-500 text-sm">
+          {new Date(date_added).toLocaleDateString()}
+        </p>
+      </div>
     </div>
   );
 };
