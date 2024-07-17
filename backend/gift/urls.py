7102e29ee649916c1d5fb42e781cd945489cb0a0
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-18 01:16:42 +0600

    added gift model related api in backend

--- backend/gift/urls.py
+++ backend/gift/urls.py
@@ -1,12 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import GiftViewSet
+from .views import (AddGiftView, BuyGiftView, DeleteGiftView,
+                    GetSpecificUserGiftView, GiftViewSet)
 
 router = DefaultRouter()
 
 router.register('list', GiftViewSet, basename='gift')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('add/', AddGiftView.as_view(), name='add_gift'),
+    path('delete/', DeleteGiftView.as_view(), name='delete_gift'),
+    path('buy/', BuyGiftView.as_view(), name='buy_gift'),
+    path('user/<int:user_id>/', GetSpecificUserGiftView.as_view(), name='user_gift'),
 ]
--- backend/gift/views.py
+++ backend/gift/views.py
@@ -1,10 +1,51 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
-from .models import Gift
-from .serializers import GiftSerializer
+from user.models import BookUser, BookUserGift
 
+from .models import Gift
+from .serializers import (AddGiftSerializer, BuyGiftSerializer,
+                          DeleteGiftSerializer, GetSpecificUserGiftSerializer,
+                          GiftSerializer)
+
+
+class AddGiftView(APIView):
+    parser_classes = (FormParser, MultiPartParser)
+
+    def post(self, request):
+        serializer = AddGiftSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DeleteGiftView(APIView):
+    def delete(self, request):
+        serializer = DeleteGiftSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.delete(serializer.validated_data)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BuyGiftView(APIView):
+    parser_classes = (FormParser, MultiPartParser)
+
+    def post(self, request):
+        serializer = BuyGiftSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class GetSpecificUserGiftView(APIView):
+    def get(self, request, user_id):
+        book_user = BookUser.objects.get(id=user_id)
+        serializer = GetSpecificUserGiftSerializer(book_user)
+        return Response(serializer.data)
 
 class GiftViewSet(viewsets.ModelViewSet):
     queryset = Gift.objects.all()
