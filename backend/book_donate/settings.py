d3f73c503ae40a63414912f9ef5c84db4ba04fc5
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-18 18:11:09 +0600

    added all user gift transform data utils

--- backend/book_donate/settings.py
+++ backend/book_donate/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'book_donate.urls'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 100,
    
 }
 
