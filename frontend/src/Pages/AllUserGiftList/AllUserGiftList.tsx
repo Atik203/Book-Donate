0a7d85ab77a5b9af448a872428c72e8103dc69d0
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-18 18:16:33 +0600

    added gift management routes

---
+++ frontend/src/Pages/AllUserGiftList/AllUserGiftList.tsx
@@ -0,0 +1,9 @@
+const AllUserGiftList = () => {
+  return (
+    <div>
+      <h1>This is AllUserGiftList component</h1>
+    </div>
+  );
+};
+
+export default AllUserGiftList;
---
+++ frontend/src/Pages/GiftList/GiftList.tsx
@@ -0,0 +1,9 @@
+const GiftList = () => {
+  return (
+    <div>
+      <h1>This is GiftList component</h1>
+    </div>
+  );
+};
+
+export default GiftList;
