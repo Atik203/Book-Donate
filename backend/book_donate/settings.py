03b9768cc25ed03f8efd7ddead8bf08ce19457f9
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-03 00:57:41 +0600

    update cors

--- backend/book_donate/settings.py
+++ backend/book_donate/settings.py
@@ -34,10 +34,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*'] # all hosts
 
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000','http://localhost:5173']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:5173','https://book-donate.vercel.app']
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173"
+    "http://localhost:5173",
+    'https://book-donate.vercel.app',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
