9835c55a59b690eb182fc0ab9dd21c713269d1b8
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-14 19:57:40 +0600

    updated image upload

---
+++ frontend/src/utils/uploadImageToImgBB.ts
@@ -0,0 +1,15 @@
+// src/utils/imgbb.ts
+import axios from "axios";
+
+const uploadImageToImgBB = async (image: File | string): Promise<string> => {
+  const formData = new FormData();
+  formData.append("image", image);
+
+  const response = await axios.post(
+    `https://api.imgbb.com/1/upload?key=${process.env.VITE_IMGBB_API_KEY}`,
+    formData
+  );
+  return response.data.data.url;
+};
+
+export default uploadImageToImgBB;
